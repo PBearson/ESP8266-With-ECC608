35e78f1ab872ef42c48e30314861dd0a943e8c15
esplab <esplab@gmail.com>
2020-09-04 10:10:33 -0400

    Code now checks status of config zone and data zone

--- main/main.c
+++ main/main.c
@@ -13,8 +13,16 @@
 
 void app_main()
 {
-	// Change this configuration as needed
-	// I2C pins are defined in components/cryptoauthlib/src/hal_esp8266_i2c.c
+	uint8_t rng[32];
+	uint8_t serialnum[9];
+	int i;
+	bool config_locked;
+	bool data_locked;
+	
+	/*
+		Change this configuration as needed
+		I2C pins are defined in components/cryptoauthlib/src/hal_esp8266_i2c.c
+	*/
 	ATCAIfaceCfg cfg = 
 	{
 		.devtype = ATECC608A,
@@ -24,10 +32,6 @@ void app_main()
 		.wake_delay = 1500,
 		.rx_retries = 20,
 	};
-
-	uint8_t rng[32];
-	uint8_t serialnum[9];
-	int i;
 
 	// Initialize the crypto chip
 	printf("Initializing chip\n");
@@ -39,6 +43,14 @@ void app_main()
 	// Get a random 32-byte number from the crypto chip
 	assert(ATCA_SUCCESS == atcab_random(rng));
 
+	// Check lock status of zones
+	assert(ATCA_SUCCESS == atcab_is_locked(LOCK_ZONE_CONFIG, &config_locked));
+	assert(ATCA_SUCCESS == atcab_is_locked(LOCK_ZONE_DATA, &data_locked));
+
+	/*
+		Print results
+	*/
+
 	printf("Your serial ID:\n");
 	for(i = 0; i < 9; i++) printf("%02X ", serialnum[i]);
 
@@ -48,4 +60,11 @@ void app_main()
 		printf("%02X ", rng[i]);
 		if((i + 1) % 8 == 0) printf("\n");
 	}
+
+	printf("\n");
+	if(config_locked) printf("Config zone is locked\n");
+	else printf("Config zone is unlocked\n");
+
+	if(data_locked) printf("Data zone is locked\n");
+	else printf("Data zone is unlocked\n");
 }
